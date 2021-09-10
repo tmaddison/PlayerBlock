cf75771ffc090a6a78606dc3e5f3caffa2c7c060
Taylor Addison <taylor.addison@turner.com>
2021-09-10 14:09:24 -0400

    adding payload

---
+++ PlayerBlock.xcframework/ios-arm64/PlayerBlock.framework/Headers/PlayerBlock.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlayerBlock.
+FOUNDATION_EXPORT double PlayerBlockVersionNumber;
+
+//! Project version string for PlayerBlock.
+FOUNDATION_EXPORT const unsigned char PlayerBlockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayerBlock/PublicHeader.h>
+
+
